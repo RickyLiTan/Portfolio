bf416c155ec6da78e2e78a6c252c541188a2dd15
RickyLiTan <rickytan1019@gmail.com>
2025-03-15 20:17:59 -0600

    Add files via upload

---
+++ scripts/openwindow.js
@@ -0,0 +1,15 @@
+function github() {
+    window.open("https://github.com/RickyLiTan");
+}
+
+function website() {
+    window.open("https://github.com/RickyLiTan/New-City-Webstite");
+}
+
+function pos() {
+    window.open("https://github.com/RickyLiTan/Restaurant-POS")
+}
+
+function linkedin() {
+    window.open("https://www.linkedin.com/in/ricky-liqiang-tan");
+}
