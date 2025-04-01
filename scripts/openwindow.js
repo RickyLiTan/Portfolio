23af73c7e0de4d9d860708f85f10aea2e34b8eea
RickyLiTan <rickytan1019@gmail.com>
2025-03-31 21:21:29 -0600

    Updates

--- scripts/openwindow.js
+++ scripts/openwindow.js
@@ -17,3 +17,14 @@ function linkedin() {
 function resume() {
     window.open("https://drive.google.com/file/d/1QEEknXOkpviydpDM2QcrDlnBe2pw-vqy/view?usp=sharing")
 }
+
+function position() {
+    var svg = document.getElementById("PROJECTS");
+    const bcr = svg.getBoundingClientRect();
+    console.log(bcr.left, bcr.top);
+
+    var svg = document.getElementById("NAV");
+    const tcr = svg.getBoundingClientRect();
+    console.log(tcr.left, tcr.top);
+    //h=631
+}
---
+++ scripts/scroll.js
@@ -0,0 +1,4 @@
+var svg = document.getElementById("NAV");
+
+const bcr = svg.target.getBoundingClientRect();
+console.log(bcr.left, bcr.top);
