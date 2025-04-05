46554534256eba7f2ba4ad8d2c9f39d2a86569b5
RickyLiTan <rickytan1019@gmail.com>
2025-04-05 13:20:08 -0600

    Hamburger Menu

---
+++ scripts/hamburger.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("MENU").style.padding = "4vw";
+    document.getElementById("MENU").style.width = "35vw";
+    document.getElementById("MENU").style.paddingTop = "2vw";
+  }
+
+function closeNav() {
+    document.getElementById("MENU").style.width = "0";
+    document.getElementById("MENU").style.padding = "0";
+    document.getElementById("MENU").style.border = "0";
+}
