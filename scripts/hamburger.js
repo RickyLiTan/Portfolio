09361f1c69b87cb6f41ce99024e570a9f0bfd3ae
RickyLiTan <rickytan1019@gmail.com>
2025-04-05 18:35:38 -0600

    Nav-bar update for mobile

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,10 @@
 function openNav() {
     document.getElementById("MENU").style.padding = "4vw";
-    document.getElementById("MENU").style.width = "35vw";
-    document.getElementById("MENU").style.paddingTop = "2vw";
+    document.getElementById("MENU").style.width = "60vw";
+    document.getElementById("MENU").style.paddingTop = "5vw";
+    document.getElementById("MENU").style.paddingRight = "5vw";
+    document.getElementById("MENU").style.borderLeft = "thin solid #070C25";
+
   }
 
 function closeNav() {
